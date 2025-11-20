4ce9b6ad18910146a99df685dcb7b72b4d0abf24
frezzz1 <skonovalov389@gmail.com>
2025-11-20 19:02:38 +0300

    add lw4

---
+++ lw4/Valuator/Pages/Summary.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StackExchange.Redis;
+namespace Valuator.Pages;
+
+public class SummaryModel : PageModel
+{
+    private readonly ILogger<SummaryModel> _logger;
+    private readonly IDatabase _db;
+
+    public SummaryModel(ILogger<SummaryModel> logger, IConnectionMultiplexer redis)
+    {
+        _logger = logger;
+        _db = redis.GetDatabase();
+    }
+
+    public double Rank { get; set; }
+    public double Similarity { get; set; }
+
+    public void OnGet(string id)
+    {
+        _logger.LogDebug(id);
+
+        // TODO: (pa1) проинициализировать свойства Rank и Similarity значениями из БД (Redis)
+        string rankKey = "RANK-" + id;
+        string similarityKey = "SIMILARITY-" + id;
+
+        string? rankValue = _db.StringGet(rankKey);
+        string? similarityValue = _db.StringGet(similarityKey);
+
+        if (rankValue != null)
+        {
+            Rank = double.Parse(rankValue);
+        }
+        else
+        {
+            Rank = -1; // Индикатор, что rank еще не вычислен
+        }
+
+        Similarity = double.TryParse(similarityValue, out double similarity) ? similarity : 0;
+    }
+
+
+}
+
